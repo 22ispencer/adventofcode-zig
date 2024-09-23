2ab66d4631baf380649463fffb85c9a527d4642a
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2024-09-23 17:28:36 -0500

    init

---
+++ day_1/part_1/src/main.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+
+    var buffered = std.io.bufferedReader(file.reader());
+    var reader = buffered.reader();
+
+    var arr = std.ArrayList(u8).init(allocator);
+    defer arr.deinit();
+
+    var sum: i32 = 0;
+    while (true) {
+        reader.streamUntilDelimiter(arr.writer(), '\n', null) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+        var firstNum: i32 = 0;
+        var lastNum: i32 = 0;
+
+        for (arr.items) |item| {
+            lastNum = switch (item) {
+                '1' => 1,
+                '2' => 2,
+                '3' => 3,
+                '4' => 4,
+                '5' => 5,
+                '6' => 6,
+                '7' => 7,
+                '8' => 8,
+                '9' => 9,
+                else => lastNum,
+            };
+            if (firstNum == 0) {
+                firstNum = lastNum;
+            }
+        }
+
+        sum += firstNum * 10 + lastNum;
+        arr.clearRetainingCapacity();
+    }
+    std.debug.print("{d}", .{sum});
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
